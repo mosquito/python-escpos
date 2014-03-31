dc83211eb3901ae809b8d77ffcbdbc0f97c28e33
Dmitry Orlov <dorlov@undev.ru>
2014-03-31 17:45:46 +0400

    [fix] setup

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ from distutils.core import setup
 
 setup(
     name='python-escpos',
-    version='1.0.3',
+    version='1.0.5',
     url='http://code.google.com/p/python-escpos',
     download_url='https://github.com/mosquito/python-escpos/archive/master.zip',
     description='Python library to manipulate ESC/POS Printers',
     license='GNU GPL v3',
-    long_description=open('README.md').read(),
     author='Manuel F Martinez',
     author_email='manpaz@bashlinux.com',
     platforms=['linux'],
     packages=[
         'escpos',
     ],
-    package_data={'': ['COPYING']},
     install_requires=[
         'pyusb',
         'Pillow>=2.0',
