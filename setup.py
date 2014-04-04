70a3c9bcd1d93a3dceca9f22b44e9abff2ea2bbb
Dmitry Orlov <dorlov@undev.ru>
2014-04-04 12:22:51 +0400

    [fix] add object for 'wich' statement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='python-escpos',
-    version='1.0.6b',
+    version='1.0.6',
     url='http://code.google.com/p/python-escpos',
     download_url='https://github.com/mosquito/python-escpos/archive/master.zip',
     description='Python library to manipulate ESC/POS Printers',
